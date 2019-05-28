3883de59bd9a49d3b263bc2657447baef9bb9545
ozinisle <ozinisle@gmail.com>
2019-05-28 14:26:20 +0530

    to be implemented as needed

--- MatrixAgentsAPI/Shared/includeHeader.php
+++ MatrixAgentsAPI/Shared/includeHeader.php
@@ -1,7 +1,7 @@
 <?php
 //DEV TEMP
 //NOTE - change the header to production url in production
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://www.techdotmasterpiece.com/*');
 header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json");
 
